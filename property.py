b9a7b16e10c61d12a8828d9dc786b7bf268431f2
b2230765023 <eed0623@gmail.com>
2024-07-14 14:36:26 +0300

    last version but still doesn't give true results

--- property.py
+++ property.py
@@ -6,20 +6,31 @@ from sklearn.preprocessing import StandardScaler
 # load data
 data_train = pd.read_csv('train_data.csv')
 
-#data_predict = pd.read_csv('predict_data.csv')
+data_predict = pd.read_csv('predict_data.csv')
+
+submission = pd.read_csv('sample_submission.csv')
 
 # data processing
 data_train.fillna(data_train.mean, inplace=True)
 data_train.drop_duplicates(inplace=True)
 
+data_predict.fillna(data_predict.mean, inplace=True)
+data_predict.drop_duplicates(inplace=True)
 
 data_train['date'] = pd.to_datetime(data_train['date'])
-data_train = pd.get_dummies(data_train, columns=['city', 'statezip'])
+#data_train = pd.get_dummies(data_train, columns=['statezip'])
+
+
+
+data_predict['date'] = pd.to_datetime(data_predict['date'])
+#data_predict = pd.get_dummies(data_predict, columns=['statezip'])
 
 
 data_train['age'] = 2024 - data_train['yr_built']
 data_train['renovated'] = data_train['yr_renovated'].apply(lambda x: 1 if x > 0 else 0)
 
+data_predict['age'] = 2024 - data_predict['yr_built']
+data_predict['renovated'] = data_predict['yr_renovated'].apply(lambda x: 1 if x > 0 else 0)
 
 scaler = StandardScaler()
 
@@ -28,9 +39,12 @@ numeric_features = ['price', 'sqft_living', 'sqft_lot', 'sqft_above', 'sqft_base
 data_train[numeric_features] = scaler.fit_transform(data_train[numeric_features])
 
 # drop non-numeric columns
-data_train = data_train.drop(columns=['street', 'country', 'date'])  # burayı droplayıp 35.satırda data.corr() yapmayı denedim ama onda da kötü bir çıktı verdi
-data_train.describe()
+data_train = data_train.drop(columns=['street', 'country','date','city','statezip']) #
+data_predict = data_predict.drop(columns=['street', 'country','date','city','statezip'])
+submission = submission.drop(columns=['price'])
 
+data_train.describe()
+data_predict.describe()
 
 # plot histograms
 plt.figure(figsize=(12, 8))
@@ -39,7 +53,7 @@ plt.tight_layout()
 plt.show()
 
 # correlation matrix
-corr_matrix = data_train.corr()
+corr_matrix = data_train[numeric_features].corr()
 
 # plot heatmap
 plt.figure(figsize=(12, 8))
@@ -56,7 +70,6 @@ from sklearn.metrics import mean_squared_error, r2_score
 X = data_train.drop('price', axis=1)
 y = data_train['price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 
 # start model
 tree_reg = DecisionTreeRegressor()
@@ -77,3 +90,13 @@ tree_reg_mse, tree_reg_r2 = evaluate_model(tree_reg, X_test, y_test)
 
 print(f'Decision Tree Regression MSE: {tree_reg_mse}, R-square: {tree_reg_r2}')
 
+predicts = tree_reg.predict(data_predict)
+print(f'Results: {predicts}')
+
+# Add the predicted prices to the prediction data DataFrame
+data_predict['predicted_price'] = predicts
+
+submission['price'] = predicts
+
+# Save the results to a CSV file
+submission.to_csv('submission.csv', index=False)
